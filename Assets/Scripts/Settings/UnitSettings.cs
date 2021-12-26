151cada76aee391d4d95fe4450413f34565c3bb7
ereona <parampareiro@mail.ru>
2021-12-26 12:15:52 +0500

    UnitSettings improvement

--- Assets/Scripts/Settings/UnitSettings.cs
+++ Assets/Scripts/Settings/UnitSettings.cs
@@ -5,9 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "SO/UnitSettings")]
 public class UnitSettings : ScriptableObject
 {
-    public int HealthPoints;
-    public int DamageAttackPoints;
-    public int AttackRange;
-    public int MoveRange;
-    public Unit Prefab;
+    [SerializeField]
+    private int _healthPoints;
+    [SerializeField]
+    private int _damageAttackPoints;
+    [SerializeField]
+    private int _attackRange;
+    [SerializeField]
+    private int _moveRange;
+    [SerializeField]
+    private Unit _prefab;
+
+    public int HealthPoints { get { return _healthPoints; } }
+    public int DamageAttackPoints { get { return _damageAttackPoints; } }
+    public int AttackRange { get { return _attackRange; } }
+    public int MoveRange { get { return _moveRange; } }
+    public Unit Prefab { get { return _prefab; } }
 }
